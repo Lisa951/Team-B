3a306998e5492e9366d7be0ace84ddd1fd9eb477
unknown <motazrj@gmail.com>
2019-10-14 18:42:15 -0400

    Added script file, ManagePic.html page and style.css

---
+++ script.js
@@ -0,0 +1,87 @@
+var AdminPhotoPath = "c:/photos/";
+function AdmincreateTable()
+{
+    var PhotoTable = document.createElement('table');
+    PhotoTable.setAttribute('id', 'PhotoTable');            // table Id
+    var tr = PhotoTable.insertRow(-1);
+
+    for (var h = 0; h < arrHead.length; h++) {
+        var th = document.createElement('th');          // the header is created here
+        th.innerHTML = arrHead[h];
+        tr.appendChild(th);
+    }
+
+    var div = document.getElementById('cont');
+    div.appendChild(PhotoTable);    // table is appended to the web page 
+   
+}
+
+function AdminaddRow() {
+    var PhotoTab = document.getElementById('PhotoTable');
+
+    var rowCnt = PhotoTab.rows.length;        // GET TABLE ROW COUNT.
+    var tr = PhotoTab.insertRow(rowCnt);
+
+    for (var c = 0; c < arrHead.length; c++) {
+        var td = document.createElement('td');
+        td = tr.insertCell(c);
+        if (c == 0) { //delete button
+            var button = document.createElement('input');
+            button.setAttribute('type', 'button');
+            button.setAttribute('value', 'Delete');
+
+            button.setAttribute('onclick', 'AdminremoveRow(this.parentNode.parentNode.rowIndex)');
+            td.appendChild(button);
+        }
+        else if (c == 1) // upload a file
+        {
+            var fil = document.createElement("input");
+            fil.setAttribute("type", "file");
+            fil.setAttribute('onchange', 'Adminupdatefunc(this.value, this.parentNode.parentNode.rowIndex)');
+            td.appendChild(fil);
+        }
+
+        else if (c == 2) //show the thumbnail
+        {
+            var img = document.createElement('img');
+            img.setAttribute('src', AdminPhotoPath + 'thumb1.jpg'); /* initial photo...TBD*/
+            img.setAttribute('width', 100);
+            td.appendChild(img);
+        }
+        else { //create a drop down list
+            var drp = document.createElement("input");
+            drp.setAttribute('type', 'select-one');
+            drp.setAttribute('onclick', 'AdminconfigDropDownLists(this.parentNode.parentNode.rowIndex,3)');
+            td.appendChild(drp);
+        }
+    }
+}
+
+function Adminupdatefunc(cel, cc) {
+    var PhotoTab = document.getElementById('PhotoTable');
+    cel = cel.replace(/^C:\\fakepath\\/, AdminPhotoPath);
+    var x = document.getElementById('PhotoTable').rows[parseInt(cc)].cells[2];
+    x.innerHTML = "<input type='image' src=" + cel + " width='100'>";
+   /* x.setAttribute('width', 100);*/
+    //alert(x.innerHTML);
+}
+
+function AdminconfigDropDownLists(rower, columner) {
+    //alert(rower);
+    var CatNames = new Array('', 'Sports', 'Pets', 'Travel', 'Nature');
+    var x = document.getElementById('PhotoTable').rows[rower].cells[3];
+    var htmls = '<select>';
+
+    for (i = 0; i < CatNames.length; i++) { //alert(i);
+        htmls += "<option value='" + CatNames[i] + "'>" + CatNames[i] + "</option>";
+    }
+    htmls += '</select>';
+    //alert(htmls);
+    x.innerHTML = htmls;
+    //alert(x);
+}
+function AdminremoveRow(rownum) {
+   // alert(rownum);
+    var rowtobedeleted = document.getElementById('PhotoTable');
+    rowtobedeleted.deleteRow(rownum);
+}

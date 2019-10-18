c6a0d25fe7b0af453454d7c65220c17a9a1b1a51
FSWD <costa.christopoulos@outlook.com>
2019-10-18 19:05:29 -0400

    Further updates to gallery.

--- script-gallery.js
+++ script-gallery.js
@@ -6,6 +6,10 @@ function galleryCanada() {
     document.getElementById("gal-desc-canada").innerHTML = "Canada is a country in the northern part of North America. Its ten provinces and three territories extend from the Atlantic to the Pacific and northward into the Arctic Ocean, covering 9.98 million square kilometres (3.85 million square miles), making it the world's second-largest country by total area. Its southern border with the United States, stretching some 8,891 kilometres (5,525 mi), is the world's longest bi-national land border. Canada's capital is Ottawa, and its three largest metropolitan areas are Toronto, Montreal, and Vancouver.";
 }
 
+function galleryUSA() {
+    document.getElementById("gal-desc-usa").innerHTML = "The United States of America (USA), commonly known as the United States (U.S. or US) or America, is a country comprising 50 states, a federal district, five major self-governing territories, and various possessions. At 3.8 million square miles (9.8 million km squared), the United States is the world's third or fourth largest country by total area and is slightly smaller than the entire continent of Europe. With a population of over 327 million people, the U.S. is the third most populous country. The capital is Washington, D.C., and the most populous city is New York City. Most of the country is located contiguously in North America between Canada and Mexico.";
+}
+
 //Breadcrumbs based on URL location
 if ($('#siteBreadcrumb ol.breadcrumb')) {
   var here = location.href.replace(/(\?.*)$/, '').split('/').slice(3);

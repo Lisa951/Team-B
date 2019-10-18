67cb52e85cbf0c4abb0bb3b800357277eb20abf4
FSWD <costa.christopoulos@outlook.com>
2019-10-18 18:10:15 -0400

    Further updates to gallery section.

--- script-gallery.js
+++ script-gallery.js
@@ -2,6 +2,10 @@ function galleryGreece() {
     document.getElementById("gal-desc-greece").innerHTML = "Greece, officially the Hellenic Republic (Greek Ελληνική Δημοκρατία), also known as Hellas (Greek Ελλάς), is a country located in Southeast Europe. Its population is approximately 10.7 million as of 2018. Athens is the nation's capital and largest city, followed by Thessaloniki.";
 }
 
+function galleryCanada() {
+    document.getElementById("gal-desc-canada").innerHTML = "Canada is a country in the northern part of North America. Its ten provinces and three territories extend from the Atlantic to the Pacific and northward into the Arctic Ocean, covering 9.98 million square kilometres (3.85 million square miles), making it the world's second-largest country by total area. Its southern border with the United States, stretching some 8,891 kilometres (5,525 mi), is the world's longest bi-national land border. Canada's capital is Ottawa, and its three largest metropolitan areas are Toronto, Montreal, and Vancouver.";
+}
+
 //Breadcrumbs based on URL location
 if ($('#siteBreadcrumb ol.breadcrumb')) {
   var here = location.href.replace(/(\?.*)$/, '').split('/').slice(3);
